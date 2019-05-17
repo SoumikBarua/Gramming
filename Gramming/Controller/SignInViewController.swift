9c91111080f3082e0df6c59484dbbc623d4738a4
SB <sb@SBs-MacBook-Pro.ThinkGuest>
2019-05-17 13:18:08 -0400

    Replaced default ViewController with custom SignInViewController, imported Firebase in AppDelegate, and updated the identity of the view controller in Main.storyboard

---
+++ Gramming/Controller/SignInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewController.swift
+//  Gramming
+//
+//  Created by SB on 5/17/19.
+//  Copyright © 2019 SB. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Custom
+    }
+}
