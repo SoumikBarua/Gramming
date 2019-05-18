36cb381ef4d233047701938acf84a85e7370e40c
SB <sb@SBs-MacBook-Pro.ThinkGuest>
2019-05-18 14:17:33 -0400

    Updated Main.storyboard to include 'or separator' between the facebook login button and the email/password text fields and implemented facebook authentication

--- Gramming/Controller/SignInViewController.swift
+++ Gramming/Controller/SignInViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInViewController: UIViewController {
     
@@ -14,4 +17,33 @@ class SignInViewController: UIViewController {
         super.viewDidLoad()
         //Custom
     }
+    
+    @IBAction func fbButtonPressed(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Problem authenticating with Facebook! - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("User cancelled Facebook authentication")
+            } else {
+                print("Successfully authenticated with Facebook!")
+                
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+            
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: {(user, error) in
+            if error != nil {
+                print("Problem authenticating with Firebase - \(error)")
+            } else {
+                print("Successfully authenticated with Firebase!")
+            }
+        })
+    }
+    
 }
